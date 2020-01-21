631d4d62b02895ae98df50ae318c87e5856c0ae2
Israel Meshileya <odunmeshileya1@gmail.com>
2020-01-21 12:24:46 +0100

    Setting up the project for the main task

---
+++ ios-coding-test/commons/DataManager/Baas.swift
@@ -0,0 +1,14 @@
+//
+//  Baas.swift
+//  ios-coding-test
+//
+//  Created by Mac on 21/01/2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct API {
+    static let baseUrl = "https://api.github.com/repos/"
+}
